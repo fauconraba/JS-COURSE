9f1627da15f914b10fdd9e3e637c6546ba4bd3f3
haytam raba <124938747+fauconraba@users.noreply.github.com>
2024-11-18 00:14:16 +0100

    Add files via upload

---
+++ 5.dom/index.js
@@ -0,0 +1,35 @@
+String(25)
+console.log('25' - 5);
+console.log('25' + 5);
+
+window.document
+window.console.log('window');
+window.alert
+
+function handleCostKeydown(event) {
+  if (event.key === 'Enter') {
+    calculateTotal();
+  }
+}
+
+function calculateTotal() {
+  const inputElement = document.querySelector('.js-cost-input');
+  let cost = Number(inputElement.value);
+
+  if (cost < 40) {
+    cost = cost + 10;
+  }
+
+  document.querySelector('.js-total-cost')
+    .innerHTML = `$${cost}`;
+}
+
+function subscribe() {
+  const buttonElement = document.querySelector('.js-subscribe-button');
+
+  if (buttonElement.innerText === 'Subscribe') {
+    buttonElement.innerHTML = 'Subscribed';
+  } else {
+    buttonElement.innerHTML = 'Subscribe';
+  }
+}
