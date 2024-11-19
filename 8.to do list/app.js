a3290c665e23530a4c287c3fb86d0082de6a74fc
haytam raba <124938747+fauconraba@users.noreply.github.com>
2024-11-19 21:44:40 +0100

    Add files via upload

---
+++ 8.to do list/app.js
@@ -0,0 +1,14 @@
+
+const list=[];
+
+
+function add() {
+
+  // get the element input and store it in the variable inptElement
+  const inptElement = document.getElementById("textInput");
+  // get the current value in the input element  and store it in inptelm 
+  list.push(inptElement.value); 
+  console.log(list);
+
+
+}
